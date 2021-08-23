21c3b9578748edeaa90c86c7064d182189cb642c
Luca Piccinelli <luca.picci@gmail.com>
2021-08-23 10:16:34 +0200

    song refactoring

--- src/test/kotlin/cgm/experiments/oak2/CsvReadTests.kt
+++ src/test/kotlin/cgm/experiments/oak2/CsvReadTests.kt
@@ -9,11 +9,11 @@ class CsvReadTests {
 
     @TestFactory
     internal fun `can read disc attributes from a csv`() = listOf(
-        "genre;Rap" to DiscAttributes.Genre("Rap"),
-        "singer;Caparezza" to DiscAttributes.Singer("Caparezza"),
-        "manager;Dario Pianta;dario.pianta@cgm.com" to DiscAttributes.Manager("Dario Pianta", "dario.pianta@cgm.com"),
-        "song;Il dito medio di Galileo;del testo a caso" to DiscAttributes.Song.NotASingle("Il dito medio di Galileo", "del testo a caso"),
-        "song;Il dito medio di Galileo;del testo a caso;1;2021-08-20" to DiscAttributes.Song.Single("Il dito medio di Galileo", "del testo a caso", LocalDate.of(2021 , 8, 20))
+        "genre;Rap" to DiscAttribute.Genre("Rap"),
+        "singer;Caparezza" to DiscAttribute.Singer("Caparezza"),
+        "manager;Dario Pianta;dario.pianta@cgm.com" to DiscAttribute.Manager("Dario Pianta", "dario.pianta@cgm.com"),
+        "song;Il dito medio di Galileo;del testo a caso" to DiscAttribute.Song.NotASingle("Il dito medio di Galileo", "del testo a caso"),
+        "song;Il dito medio di Galileo;del testo a caso;1;2021-08-20" to DiscAttribute.Song.Single("Il dito medio di Galileo", "del testo a caso", LocalDate.of(2021 , 8, 20))
     )
     .map { (inputStr, expectedResult) ->
         dynamicTest("given $inputStr i expect $expectedResult") {
@@ -25,20 +25,26 @@ class CsvReadTests {
 }
 
 object CsvReader{
-    fun read(inputStr: String): DiscAttributes? {
+    fun read(inputStr: String): DiscAttribute? {
         val elements: List<String> = inputStr.split(";")
-        return DiscAttributes.of(elements.first(), elements.drop(1))
+        return DiscAttribute.of(elements.first(), elements.drop(1))
     }
 
 }
 
-sealed class DiscAttributes{
-    data class Singer(val stageName: String): DiscAttributes()
-    data class Genre(val name: String): DiscAttributes()
-    data class Manager(val name: String, val email: String) : DiscAttributes()
-    sealed class Song(open val title: String, open val lyrics: String) : DiscAttributes(){
+sealed class DiscAttribute{
+    data class Singer(val stageName: String): DiscAttribute()
+    data class Genre(val name: String): DiscAttribute()
+    data class Manager(val name: String, val email: String) : DiscAttribute()
+    sealed class Song(open val title: String, open val lyrics: String) : DiscAttribute(){
         data class Single(override val title: String, override val lyrics: String, val singleDate: LocalDate): Song(title, lyrics)
         data class NotASingle(override val title: String, override val lyrics: String): Song(title, lyrics)
+
+        companion object {
+            fun of(attributes: List<String>) =
+                if (attributes.size > 2) Single(attributes[0], attributes[1], LocalDate.parse(attributes[3]))
+                else NotASingle(attributes[0], attributes[1])
+        }
     }
 
     companion object {
@@ -46,26 +52,9 @@ sealed class DiscAttributes{
             "genre" -> Genre(attributes[0])
             "singer" -> Singer(attributes[0])
             "manager" -> Manager(attributes[0], attributes[1])
-            "song" ->
-                if(attributes.size > 2) Song.Single(attributes[0], attributes[1], LocalDate.parse(attributes[3]))
-                else Song.NotASingle(attributes[0], attributes[1])
+            "song" -> Song.of(attributes)
             else -> null
         }
+
     }
-}
-
-fun soundASingle(song: DiscAttributes.Song.Single){
-    println("sounding this song: ${song.title} released on ${song.singleDate}")
-}
-
-fun listenASong(song: DiscAttributes.Song){
-    println("sounding this song: ${song.title}")
-}
-
-fun emailASong(song: DiscAttributes.Song.Single){
-    emailme("sounding this song: ${song.title} released on ${song.singleDate}")
-}
-
-fun emailme(s: String) {
-    TODO("Not yet implemented")
 }
